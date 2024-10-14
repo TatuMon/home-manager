bbfab7ae3bd5ba114c824b409c37d3cb84babea0
tatumon <tatumonar@proton.me>
2024-10-14 12:03:18 -0300

    modified user home directory

--- home.nix
+++ home.nix
@@ -87,7 +87,7 @@ in {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "tatumon";
-  home.homeDirectory = "/home/tatumon";
+  home.homeDirectory = "/Users/tatumon";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
