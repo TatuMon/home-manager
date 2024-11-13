7b91eea45897f2cb927480d2aa088bc3da005496
tatumon <tatumonar@proton.me>
2024-11-13 15:00:23 -0300

    nixvim: Set LSP borders

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -64,6 +64,29 @@ pkgs: {
         cmake.enable = true; # CMake
         dockerls.enable = true; # Dockerfile
       };
+      extraConfigLua = ''
+        local _border = "rounded"
+
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+          vim.lsp.handlers.hover, {
+            border = _border
+          }
+        )
+
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+          vim.lsp.handlers.signature_help, {
+            border = _border
+          }
+        )
+
+        vim.diagnostic.config{
+          float={border=_border}
+        };
+
+        require('lspconfig.ui.windows').default_options = {
+          border = _border
+        }
+      '';
     };
 
     typescript-tools.enable = true;
@@ -123,7 +146,7 @@ pkgs: {
 
     nvim-autopairs = { enable = true; };
 
-    nvim-tree = { enable = true; };
+    neo-tree = { enable = true; };
 
     telescope = { enable = true; };
 
