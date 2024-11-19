b48fee1cce2d3d2cad49cd071dde18f4dccb44b4
tatumon <tatumonar@proton.me>
2024-11-19 13:34:05 -0300

    kitty: Removed FiraCode font family

--- home.nix
+++ home.nix
@@ -29,7 +29,7 @@ in {
   programs.kitty = {
     enable = true;
     font = {
-      name = "GohuFont uni14 Nerd Font";
+      name = "GohuFont uni14 Nerd Font Mono";
     };
     extraConfig = ''
       background_opacity 0.98
