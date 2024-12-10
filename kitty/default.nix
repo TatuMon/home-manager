82baa4c9654e4f4094ade55b7f0fee6f37a8ff4a
tatumon <tatumonar@proton.me>
2024-12-10 11:45:04 -0300

    kitty: More opacity

--- kitty/default.nix
+++ kitty/default.nix
@@ -2,7 +2,7 @@ nixpkgs2411 : {
   enable = true;
   font = { name = "GohuFont uni14 Nerd Font Mono"; };
   settings = {
-    background_opacity = "0.70";
+    background_opacity = "0.9";
   };
   package = nixpkgs2411.pkgs.kitty;
 }
