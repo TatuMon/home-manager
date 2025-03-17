8aec185c9ab5d115341dbceac85ab62141e0076f
tatumon <tatumonar@proton.me>
2025-03-17 16:04:05 -0300

    nixvim: Forgot to remove some configuration

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -134,10 +134,6 @@ pkgs: {
             })
           '';
         };
-        snippet = {
-          expand =
-            "function(args) require('luasnip').lsp_expand(args.body) end";
-        };
         sources = [
           { name = "nvim_lsp"; }
           { name = "vsnip"; }
