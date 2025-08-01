44cd724f46f8db328a11a62242a7a5346c86fc3b
tatumon <tatumonar@proton.me>
2025-08-01 18:56:26 -0300

    swaync: New config

--- home.nix
+++ home.nix
@@ -181,6 +181,7 @@ in {
     ".config/hypr".source = ./hyprland;
     ".config/waybar".source = ./waybar;
     ".config/wlogout".source = ./wlogout;
+    ".config/swaync".source = ./swaync;
   };
 
   # Home Manager can also manage your environment variables through
