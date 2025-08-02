c4878c6c489454cc7a6328318271c02e5288490f
tatumon <tatumonar@proton.me>
2025-08-02 02:48:36 -0300

    rofi: super+shift+tab to open window selector

--- rofi/default.nix
+++ rofi/default.nix
@@ -2,6 +2,12 @@ pkgs: {
   enable = true;
   plugins = [ pkgs.rofi-calc ];
   theme = "squared-nord";
+  modes = [
+    "drun"
+    "calc"
+    "filebrowser"
+    "window"
+  ];
   extraConfig = {
     drun-display-format = "{name}";
     display-drun = "󰍉";
