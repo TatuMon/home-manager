ae436d4f5af1e6221ee6e8b08e0f5cee7c989fdf
tatumon <tatumonar@proton.me>
2025-08-08 11:54:49 -0300

    xdg: Set Brave as default browser

--- xdg/default.nix
+++ xdg/default.nix
@@ -3,6 +3,11 @@
   mimeApps = {
     enable = true;
     defaultApplications = {
+      "text/html" = ["brave-browser.desktop"];
+      "x-scheme-handler/http" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/https" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/about" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/unknown" = [ "brave-browser.desktop" ];
       "image/png" = [ "brave-browser.desktop" ];
       "image/jpeg" = [ "brave-browser.desktop" ];
       "image/webp" = [ "brave-browser.desktop" ];
