d983ee112f8b0f826e8139d9b60a9a13426e4b18
tatumon <tatumonar@proton.me>
2025-08-10 18:49:10 -0300

    xdg: Set pcmanfm as default file manager

--- xdg/default.nix
+++ xdg/default.nix
@@ -3,7 +3,8 @@
   mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/html" = ["brave-browser.desktop"];
+      "inode/directory" = [ "pcmanfm.desktop" ];
+      "text/html" = [ "brave-browser.desktop" ];
       "x-scheme-handler/http" = [ "brave-browser.desktop" ];
       "x-scheme-handler/https" = [ "brave-browser.desktop" ];
       "x-scheme-handler/about" = [ "brave-browser.desktop" ];
