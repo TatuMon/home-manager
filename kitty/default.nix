a9be5ad06323a306a220d7ccb3b9d315925830e8
tatumon <tatumonar@proton.me>
2025-08-30 20:07:10 -0300

    kitty: Reduced font size

--- kitty/default.nix
+++ kitty/default.nix
@@ -2,7 +2,7 @@
   enable = true;
   settings = {
     font_family = "JetBrainsMono Nerd Font";
-    font_size = "14";
+    font_size = "10";
     background_opacity = "0.9";
   };
   package = config.lib.nixGL.wrap pkgs.kitty;
