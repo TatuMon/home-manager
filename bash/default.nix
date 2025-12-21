37841ce9dc2d946d26034f95d8fb810cb25fd0d8
tatumon <tatumonar@proton.me>
2025-12-21 16:42:53 -0300

    Now I handle Golang with asdf

--- bash/default.nix
+++ bash/default.nix
@@ -1,16 +1,11 @@
 {
   enable = true;
   profileExtra = ''
-    export GOPATH=$HOME/go
-
     # asdf
     export PATH="$PATH:$HOME/.asdf/shims"
 
     # Applications
     export PATH="$PATH:$HOME/Applications"
-
-    # Go path
-    export PATH="$PATH:$GOPATH/bin"
   '';
   bashrcExtra = ''
     tmux a || tmux
