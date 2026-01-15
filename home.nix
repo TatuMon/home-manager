b14a4e2f315533ab4fa1aef5e4adb0d320308e84
tatumon <tatumonar@proton.me>
2026-01-15 17:27:35 -0300

    Removed Microsoft teams for linux
    I don't use it anymore

--- home.nix
+++ home.nix
@@ -96,11 +96,6 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    ############
-    ### WORK ###
-    ############
-    teams-for-linux
-
     ################
     ### SOFTWARE ###
     ################
