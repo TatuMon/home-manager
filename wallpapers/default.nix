cd867abef6743ca83c1d2f6ab62f5b830b319d35
tatumon <tatumonar@proton.me>
2026-02-01 20:05:10 -0300

    Pinning my wallpapers repo

--- wallpapers/default.nix
+++ wallpapers/default.nix
@@ -1,24 +1,26 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 let
-    repoUrl = "https://github.com/TatuMon/wallpapers";
-    repoName = "wallpapers";
+  repoUrl = "https://github.com/TatuMon/wallpapers";
+  repoName = "wallpapers";
 in
 pkgs.stdenv.mkDerivation {
-    name = "tatumon-wallpapers-collection";
-    src = builtins.fetchGit {
-        url = repoUrl;
-        name = repoName;
-    };
+  name = "tatumon-wallpapers-collection";
+  src = builtins.fetchGit {
+    url = repoUrl;
+    name = repoName;
+    rev = "58333b0a3a3d410c8112f3cb5542bbe7ddadcf55";
+  };
 
-    dontBuild = true;
-    dontConfigure = true;
-    preferLocalBuild = true;
-    allowSubstitutes = false;
+  dontBuild = true;
+  dontConfigure = true;
+  preferLocalBuild = true;
+  allowSubstitutes = false;
 
-    installPhase = ''
-        mkdir -p $out/wallpapers
-        cp -r $src/* $out/wallpapers/
-    '';
+  installPhase = ''
+    mkdir -p $out/wallpapers
+    cp -r $src/* $out/wallpapers/
+  '';
 }
-
